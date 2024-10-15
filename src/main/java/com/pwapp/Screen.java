29a27d77271d450b1a995ca02bc6bae9a44ff460
d-quint <112879320+d-quint@users.noreply.github.com>
2024-10-15 14:48:31 +0800

    remove unused imports

--- src/main/java/com/pwapp/Screen.java
+++ src/main/java/com/pwapp/Screen.java
@@ -4,8 +4,6 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.regex.Pattern;
 
 /**
